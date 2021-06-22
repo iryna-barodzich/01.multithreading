be517887d8822e3a15a10cfff1c66bcf389c134d
Iryna Barodzich <Iryna_Barodzich@epam.com>
2021-06-22 23:00:51 +0300

    task1

---
+++ ExpressionTrees.Task1.ExpressionsTransformator/IncDecExpressionVisitor.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq.Expressions;
+
+namespace ExpressionTrees.Task1.ExpressionsTransformer
+{
+    public class IncDecExpressionVisitor : ExpressionVisitor
+    {
+        public int indent = 0;
+
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            if (node.NodeType == ExpressionType.Add)
+            {
+                ParameterExpression param = null;
+                ConstantExpression constant = null;
+                if (node.Left.NodeType == ExpressionType.Parameter)
+                {
+                    param = (ParameterExpression)node.Left;
+                }
+                else if (node.Left.NodeType == ExpressionType.Constant)
+                {
+                    constant = (ConstantExpression)node.Left;
+                }
+
+                if (node.Right.NodeType == ExpressionType.Parameter)
+                {
+                    param = (ParameterExpression)node.Right;
+                }
+                else if (node.Right.NodeType == ExpressionType.Constant)
+                {
+                    constant = (ConstantExpression)node.Right;
+                }
+
+                if (param != null && constant != null && constant.Type == typeof(int) && (int)constant.Value == 1)
+                {
+                    return Expression.Increment(param);
+                }
+            }
+            else if(node.NodeType == ExpressionType.Subtract)
+            {
+                ParameterExpression param = null;
+                ConstantExpression constant = null;
+                if (node.Left.NodeType == ExpressionType.Parameter)
+                {
+                    param = (ParameterExpression)node.Left;
+                }
+
+                if (node.Right.NodeType == ExpressionType.Constant)
+                {
+                    constant = (ConstantExpression)node.Right;
+                }
+
+                if (param != null && constant != null && constant.Type == typeof(int) && (int)constant.Value == 1)
+                {
+                    return Expression.Decrement(param);
+                }
+            }
+            return base.VisitBinary(node);
+
+        }
+    }
+}
---
+++ ExpressionTrees.Task1.ExpressionsTransformator/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Create a class based on ExpressionVisitor, which makes expression tree transformation:
+ * 1. converts expressions like <variable> + 1 to increment operations, <variable> - 1 - into decrement operations.
+ * 2. changes parameter values in a lambda expression to constants, taking the following as transformation parameters:
+ *    - source expression;
+ *    - dictionary: <parameter name: value for replacement>
+ * The results could be printed in console or checked via Debugger using any Visualizer.
+ */
+using System;
+using System.Linq.Expressions;
+
+namespace ExpressionTrees.Task1.ExpressionsTransformer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Expression Visitor for increment/decrement.");
+            Console.WriteLine();
+
+            Expression<Func<int, int>> expression = (num) => num + (num + 1) + (num + 2) + (num - 1);
+            var result = (new IncDecExpressionVisitor().VisitAndConvert(expression, string.Empty));
+            Console.WriteLine(expression);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
